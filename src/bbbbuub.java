b67766750388e29cc924dddb8de80060a8e67882
Timur <kyrgyibekovtimur@gmail.com>
2026-02-23 13:22:32 +0600

    added enum and class

---
+++ src/bbbbuub.java
@@ -0,0 +1,45 @@
+public class bbbbuub {
+    public int price;
+    public String uhhyyhh;
+    public Toys toys;
+
+
+    public bbbbuub(int price, String uhhyyhh, Toys toys) {
+        this.price = price;
+        this.uhhyyhh = uhhyyhh;
+        this.toys = toys;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public String getUhhyyhh() {
+        return uhhyyhh;
+    }
+
+    public void setUhhyyhh(String uhhyyhh) {
+        this.uhhyyhh = uhhyyhh;
+    }
+
+    public Toys getToys() {
+        return toys;
+    }
+
+    public void setToys(Toys toys) {
+        this.toys = toys;
+    }
+
+    @Override
+    public String toString() {
+        return "bbbbuub{" +
+                "price=" + price +
+                ", uhhyyhh='" + uhhyyhh + '\'' +
+                ", toys=" + toys +
+                '}';
+    }
+}
